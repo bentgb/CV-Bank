8bb0923ef2ed9d425a1d23d3c5e39be0b04bf622
Tugba A <karaca.tgb@gmail.com>
2020-08-27 11:35:55 +0200

    added new columns to user table in database

--- backend/dataDB.js
+++ backend/dataDB.js
@@ -13,17 +13,18 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         db.run('CREATE TABLE USERS (' +
             'userId INTEGER PRIMARY KEY,' +
             'user TEXT,passCODE TEXT,' +
+            'age INTEGER,class TEXT,' +
             'userEMAIL TEXT )', (err) => {
             if (err) {
                 // Table already created
             } else {
                 // Table just created, creating some rows
-                var insert = 'INSERT INTO USERS (user,passCODE,userEMAIL) VALUES(?,?,?)'
-                db.run(insert, ["Aisha", "1234", "aisha@test.com"])
-                db.run(insert, ["Tugba", "1234", "tugba@test.com"])
-                db.run(insert, ["Husamettin", "1234", "husamettin@test.com"])
-                db.run(insert, ["Fatma", "1234", "fatma@test.com"])
-                db.run(insert, ["Jack", "1234", "jack@test.com"])
+                var insert = 'INSERT INTO USERS (user,passCODE,age,class,userEMAIL) VALUES(?,?,?,?,?)'
+                db.run(insert, ["Aisha", "1234", 35, "Java", "aisha@test.com"])
+                db.run(insert, ["Tugba", "1234", 37, "Java", "tugba@test.com"])
+                db.run(insert, ["Husamettin", "1234",37, "Java", "husamettin@test.com"])
+                db.run(insert, ["Fatma", "1234", 37, "Java","fatma@test.com"])
+                db.run(insert, ["Edwin", "1234", 27, "Java","edwin@test.com"])
 
 
             }
