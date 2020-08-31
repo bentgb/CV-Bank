ccf569f7d6c1164c38e89d4a308831727170f2ab
Tugba A <karaca.tgb@gmail.com>
2020-08-31 18:05:52 +0200

    get methods added to github

--- backend/dataDB.js
+++ backend/dataDB.js
@@ -14,18 +14,18 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
             'userId INTEGER PRIMARY KEY,' +
             'user TEXT,passCODE TEXT,' +
             'age INTEGER,class TEXT,' +
+            'userRole TEXT,' +
             'userEMAIL TEXT )', (err) => {
 
             if (err) {
                 // Table already created
             } else {
                 // Table just created, creating some rows
-                var insert = 'INSERT INTO USERS (user,passCODE,age,class,userEMAIL) VALUES(?,?,?,?,?)'
-                db.run(insert, ["Aisha", "1234", 35, "Java", "aisha@test.com"])
-                db.run(insert, ["Tugba", "1234", 37, "Java", "tugba@test.com"])
-                db.run(insert, ["Husamettin", "1234",37, "Java", "husamettin@test.com"])
-                db.run(insert, ["Fatma", "1234", 37, "Java","fatma@test.com"])
-                db.run(insert, ["Edwin", "1234", 27, "Java","edwin@test.com"])
+                var insert = 'INSERT INTO USERS (user,passCODE,age,class,userRole, userEMAIL) VALUES(?,?,?,?,?,?)'
+                db.run(insert, ["Aisha", "1234", 35, "Java", "student", "aisha@test.com"])
+                db.run(insert, ["Tugba", "1234", 37, "Java", "admin", "tugba@test.com"])
+                db.run(insert, ["Husamettin", "1234",37, "Java","student", "husamettin@test.com"])
+                db.run(insert, ["Edwin", "1234", 27, "Java","student","edwin@test.com"])
 
 
             }
