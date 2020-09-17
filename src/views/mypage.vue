7db0cdeb84c3e2f29a4169d2cccf949d4725f4e4
Edwin Berg <e.berg117@gmail.com>
2020-09-17 13:48:39 +0200

    merge

--- src/views/mypage.vue
+++ src/views/mypage.vue
@@ -10,10 +10,10 @@
 
     <div class="mt-4">
       <b-card
-          :img-src="require('../assets/' + form_img)"
-          :img-alt="'No image selected'"
-          img-left
-          class="mb-3"
+        :img-src="`../assets/${ form_img }`"
+        img-alt="No image selected"
+        img-left
+        class="mb-3"
       >
         <b-card-title class="text-center mr-5">{{ this.$parent.user.user }}</b-card-title>
         <b-card-sub-title class="text-left ml-2 mb-2">Ålder</b-card-sub-title>
@@ -28,73 +28,83 @@
 
       <b-form-group class="text-left" label="Description" label-for="textarea-lazy">
         <b-form-textarea
-            v-model="desc"
-            id="textarea-lazy"
-            :placeholder="this.$parent.user.description"
-            lazy-formatter
-            :formatter="formatter"
+          v-model="desc"
+          id="textarea-lazy"
+          :placeholder="this.$parent.user.description"
+          lazy-formatter
         ></b-form-textarea>
         <b-button class="mr-0 mt-1" size="sm" @click="saveDescription()">Save</b-button>
       </b-form-group>
       <h5 class="text-left">Upload CV</h5>
 
       <b-form-group id="image-group">
-        <b-form-file name="resume"
-            accept=".pdf"
-            enctype="multipart/form-data"
-            ref="file"
-            v-model="file"
-            v-on:change="handleFileUpload()"
-            placeholder="Select a pdf file or drop it here ..."
-            drop-placeholder="Släpp .pdf filen här som ..."
+        <b-form-file
+          name="resume"
+          accept=".pdf"
+          enctype="multipart/form-data"
+          ref="file"
+          v-model="file"
+          v-on:change="handleFileUpload()"
+          placeholder="Select a pdf file or drop it here ..."
+          drop-placeholder="Släpp .pdf filen här som ..."
         />
         <b-button class="mr-0 mt-1" size="sm" v-on:click="submitFile()">Save</b-button>
-        <a href="" v-if="resumeExists" v-on:click="readResume()"> You already have an uploaded CV. Would u like to see it?</a>
+        <a
+          href
+          v-if="resumeExists"
+          v-on:click="readResume()"
+        >You already have an uploaded CV. Would u like to see it?</a>
       </b-form-group>
       <div v-if="fileUploaded">
-     {{ this.file.name }} is uploaded
-
-<!--                <a -->
-<!--                    href=`http://localhost:3000/api/uploads/resumes/${this.$parent.user.userId}`-->
-<!--                >Mypdf</a>-->
+        {{ this.file.name }} is uploaded
+        <!--                <a -->
+        <!--                    href=`http://localhost:3000/api/uploads/resumes/${this.$parent.user.userId}`-->
+        <!--                >Mypdf</a>-->
         <!--
         <iframe :src="getPDFPath()" style="width:200px; height:200px; border: none;">
           Oops! an error has occurred.
         </iframe>
         -->
-        <b-button class="mr-0 mt-1" size="sm" v-on:click="readResume()">Open</b-button>
+        <b-button class="mr-0 mt-1" size="sm" @click="showModal">Open</b-button>
       </div>
 
+      <div>
+        <b-modal ref="my-modal" hide-footer>
+          <div class="d-block text-center">
+            <b-embed type="iframe" aspect="16by9" :src="`${ readResume() }`"></b-embed>
+          </div>
+        </b-modal>
+      </div>
       <h5 class="text-left">Upload cover letter</h5>
-      <b-form-file name="coverletter"
-                   accept=".pdf"
-                   enctype="multipart/form-data"
-                   ref="file"
-                   v-model="file"
-                   v-on:change="handleFileUpload()"
-                   placeholder="Select a pdf file or drop it here ..."
-                   drop-placeholder="Släpp .pdf filen här som ..."
+      <b-form-file
+        name="coverletter"
+        accept=".pdf"
+        enctype="multipart/form-data"
+        ref="file"
+        v-model="file"
+        v-on:change="handleFileUpload()"
+        placeholder="Select a pdf file or drop it here ..."
+        drop-placeholder="Släpp .pdf filen här som ..."
       ></b-form-file>
       <b-button class="mr-0 mt-1" size="sm" v-on:click="submitCoverLetter()">Save</b-button>
       <div v-if="fileUploaded">
         {{ this.file.name }} is uploaded
-
         <b-button class="mr-0 mt-1" size="sm" v-on:click="readCoverLetter()">Open</b-button>
       </div>
       <h5 class="text-left">Upload Certicate</h5>
-      <b-form-file name="certificate"
-                   accept=".pdf"
-                   enctype="multipart/form-data"
-                   ref="file"
-                   v-model="file"
-                   v-on:change="handleFileUpload()"
-                   placeholder="Select a pdf file or drop it here ..."
-                   drop-placeholder="Släpp .pdf filen här som ..."
+      <b-form-file
+        name="certificate"
+        accept=".pdf"
+        enctype="multipart/form-data"
+        ref="file"
+        v-model="file"
+        v-on:change="handleFileUpload()"
+        placeholder="Select a pdf file or drop it here ..."
+        drop-placeholder="Släpp .pdf filen här som ..."
       ></b-form-file>
       <b-button class="mr-0 mt-1" size="sm" v-on:click="submitCertificate()">Save</b-button>
       <div v-if="fileUploaded">
         {{ this.file.name }} is uploaded
-
         <b-button class="mr-0 mt-1" size="sm" v-on:click="readCertificate()">Open</b-button>
       </div>
     </div>
@@ -108,11 +118,11 @@ export default {
   data() {
     return {
       desc: "",
-      file: "",
+      file: null,
       fileUploaded: false,
-      resumeExists:false,
+      resumeExists: false,
       message: "test",
-      form_img: "test.png",
+      form_img: null,
       img: null
     };
   },
@@ -121,32 +131,33 @@ export default {
       this.file = this.$refs.file.files[0];
     },
 
-    checkResume(){
-      var myResume = new File(`../resumes/${this.$parent.user.userId}resume.pdf`);
+    checkResume() {
+      var myResume = new File(
+        `../resumes/${this.$parent.user.userId}resume.pdf`
+      );
 
-
-// See if the file exists
-      if(myResume.exists()){
-        this.resumeExists=true;
-      }else{
-        console.log('The file does not exist');
+      // See if the file exists
+      if (myResume.exists()) {
+        this.resumeExists = true;
+      } else {
+        console.log("The file does not exist");
       }
-
-
     },
-
 
     async submitFile() {
       const formData = new FormData();
       formData.append("resume", this.file);
       try {
         if (this.file != "") {
-          axios.post("http://127.0.0.1:3000/api/upload/resume/" + this.$parent.user.userId, formData);
+          axios.post(
+            "http://127.0.0.1:3000/api/upload/resume/" +
+              this.$parent.user.userId,
+            formData
+          );
           this.message = "uploaded";
           this.fileUploaded = true;
         } else {
           alert("Choose a File ");
-
         }
       } catch (e) {
         this.message = "Sth went wrong";
@@ -158,13 +169,16 @@ export default {
       formData.append("coverletter", this.file);
       try {
         if (this.file != "") {
-          axios.post("http://127.0.0.1:3000/api/upload/coverletter/" + this.$parent.user.userId, formData);
+          axios.post(
+            "http://127.0.0.1:3000/api/upload/coverletter/" +
+              this.$parent.user.userId,
+            formData
+          );
           this.message = "uploaded";
           this.fileUploaded = true;
           alert("cover letter uploaded");
         } else {
           alert("Choose a File ");
-
         }
       } catch (e) {
         this.message = "Sth went wrong";
@@ -176,13 +190,16 @@ export default {
       formData.append("certificate", this.file);
       try {
         if (this.file != "") {
-          axios.post("http://127.0.0.1:3000/api/upload/certificate/" + this.$parent.user.userId, formData);
+          axios.post(
+            "http://127.0.0.1:3000/api/upload/certificate/" +
+              this.$parent.user.userId,
+            formData
+          );
           this.message = "uploaded";
           this.fileUploaded = true;
           alert("cover letter uploaded");
         } else {
           alert("Choose a File ");
-
         }
       } catch (e) {
         this.message = "Sth went wrong";
@@ -193,12 +210,12 @@ export default {
       const requestOptions = {
         method: "PUT",
         mode: "cors",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({description: this.desc})
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ description: this.desc })
       };
       const response = await fetch(
-          "http://localhost:3000/api/users/" + this.$parent.user.userId,
-          requestOptions
+        "http://localhost:3000/api/users/" + this.$parent.user.userId,
+        requestOptions
       );
       console.log(this.$parent.user.userId);
       const data = await response.json();
@@ -208,22 +225,32 @@ export default {
     },
 
     readResume() {
-      window.open("http://localhost:3000/api/uploads/resumes/" + this.$parent.user.userId, "_blank"); //to open in new tab
+      "http://localhost:3000/api/uploades/resumes/" + this.$parent.user.userId; //to open in new tab
     },
 
     readCoverLetter() {
-      window.open("http://localhost:3000/api/uploads/coverletters/" + this.$parent.user.userId, "_blank"); //to open in new tab
+      window.open(
+        "http://localhost:3000/api/uploads/coverletters/" +
+          this.$parent.user.userId,
+        "_blank"
+      ); //to open in new tab
     },
-
 
     readCertificate() {
-      window.open("http://localhost:3000/api/uploads/certificates/" + this.$parent.user.userId, "_blank"); //to open in new tab
+      window.open(
+        "http://localhost:3000/api/uploads/certificates/" +
+          this.$parent.user.userId,
+        "_blank"
+      ); //to open in new tab
     },
 
-
-    image() {
-      this.form_img = this.img.name;
+    showModal() {
+      this.$refs["my-modal"].show();
+    },
+    hideModal() {
+      this.$refs["my-modal"].hide();
     }
+
     // fetch("api/users", {
     //   body: JSON.stringify({
     //     form_img: this.img.name
@@ -243,7 +270,6 @@ export default {
       return './uploads/Aisha.pdf'
 
     },*/
-
-}
+};
 </script>
 
