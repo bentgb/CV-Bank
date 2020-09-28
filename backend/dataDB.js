7c8a315a391279969b3b391a7fa9651bf2bba12e
ha49 <kaysersoza@outlook.com>
2020-09-28 09:43:49 +0200

    java class modified

--- backend/dataDB.js
+++ backend/dataDB.js
@@ -34,7 +34,12 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                 db.run(insert, [30, "Husam", "1234",37, "Java","student", "I am Husamettin. I am Java developer. I want to get job.","husam@test.com", "", "test.jpg"])
                 db.run(insert, [40, "Edwin", "1234", 27, "Web","student","I like developing web applications", "edwin@test.com", "", "test.jpg"])
                 db.run(insert, [50, "Joseph", "1234", 27, "Tester","student","I am a tester", "edwin@test.com", "", "test.jpg"])
-                db.run(insert, [60, "Azmat", "1234", 27, "Tester","student","I am a tester", "edwin@test.com", "", "test.jpg"])
+                db.run(insert, [51, "Azmat", "1234", 27, "Tester","student","I am a tester", "Azmat@test.com", "", "test.jpg"])
+                db.run(insert, [52, "Mark", "1234", 35, "Java", "student", "Hello" ,"Mark@test.com", "", "test.jpg"])
+                db.run(insert, [53, "Tim", "1234",37, "Java","student", "Hej","Mark@test.com", "", "test.jpg"])
+                db.run(insert, [54, "Sara", "1234", 27, "Java","student","Hello from Sara", "Sara@test.com", "", "test.jpg"])
+                db.run(insert, [55, "Eva", "1234", 27, "Web","student","I am a tester", "Eva@test.com", "", "test.jpg"])
+                db.run(insert, [56, "Can", "1234", 27, "Tester","student","I am a tester", "Can@test.com", "", "test.jpg"])
 
             }
         })
